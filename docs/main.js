316b2110035d60d080278377ba0bd7f14d72773e
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-07-26 01:01:45 +0900

    Create main.js

---
+++ docs/main.js
@@ -0,0 +1,33 @@
+const myId = (new MediaStream).id;
+console.log(`myId:${myId}`);
+let stream = null;
+const skywayApiKey = '6116551f-321e-4a11-9cac-d2650671232a';
+function appendVideo(stream) {
+    const video = document.createElement('video');
+    video.srcObject = stream;
+    document.body.appendChild(video);
+    video.play();
+}
+const constraints = {
+    video: true
+};
+navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+    console.log(`streamId:${stream.id}`);
+    appendVideo(stream);
+    const peer = new Peer(myId, {
+        key: skywayApiKey 
+    });
+    peer.on('open', id => {
+        myIdDisp.textContent = id;
+        const room = peer.joinRoom('hoge_fuga_piyo_sfu', { mode: 'sfu', stream });
+        room.on('stream', stream => {
+            console.log(`room on stream peerId:${stream.peerId}`);
+            appendVideo(stream);
+            const dummyPeer = new Peer({ key: skywayApiKey });
+            dummyPeer.on('open', _ => dummyPeer.close());
+            dummyPeer.on('error', err => console.error(err));
+        });
+    });
+}).catch(err => {
+    console.error(err);
+});
